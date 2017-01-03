d5ccd8b306ee7d588c971501d7b9ba4605771cb9
kristokuiv <kristokuiv@gmail.com>
2017-01-03 17:55:37 +0200

    Start NearestActivity when clicking 'Up' button on profile view to avoid going back to login view after signing in with username/password

--- app/src/main/java/ee/ajapaik/android/ProfileActivity.java
+++ app/src/main/java/ee/ajapaik/android/ProfileActivity.java
@@ -3,11 +3,10 @@ package ee.ajapaik.android;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import ee.ajapaik.android.fragment.ProfileFragment;
-import ee.ajapaik.android.util.WebActivity;
-
 import ee.ajapaik.android.test.R;
+import ee.ajapaik.android.util.WebActivity;
 
 public class ProfileActivity extends WebActivity {
     public static void start(Context context) {
@@ -24,5 +23,13 @@ public class ProfileActivity extends WebActivity {
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.container, new ProfileFragment()).commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.startActivity(new Intent(this, NearestActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
