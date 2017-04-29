51eed5bf4abb84e603325a0a89ef8584ef81dccc
kristokuiv <kristokuiv@gmail.com>
2017-04-29 13:43:55 +0300

    Show actual version number on about page

--- app/src/main/java/ee/ajapaik/android/AboutActivity.java
+++ app/src/main/java/ee/ajapaik/android/AboutActivity.java
@@ -2,9 +2,12 @@ package ee.ajapaik.android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import ee.ajapaik.android.test.R;
 
 public class AboutActivity extends NavigationDrawerActivity implements View.OnClickListener {
@@ -20,6 +23,12 @@ public class AboutActivity extends NavigationDrawerActivity implements View.OnCl
         setContentView(R.layout.activity_about);
         configureNavigationDrawer();
         configureToolbar();
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            ((TextView)findViewById(R.id.version_value)).setText(pInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
