484e93a6095eba044c49f10ec0088bac6ed815d8
kristokuiv <kristokuiv@gmail.com>
2017-04-29 13:35:36 +0300

    Clicking on "privacy" on about page opens browser and https://www.ajapaik.ee

--- app/src/main/java/ee/ajapaik/android/AboutActivity.java
+++ app/src/main/java/ee/ajapaik/android/AboutActivity.java
@@ -2,10 +2,12 @@ package ee.ajapaik.android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import ee.ajapaik.android.test.R;
 
-public class AboutActivity extends NavigationDrawerActivity {
+public class AboutActivity extends NavigationDrawerActivity implements View.OnClickListener {
 
     public static void start(Context context) {
         Intent intent = new Intent(context, AboutActivity.class);
@@ -18,5 +20,13 @@ public class AboutActivity extends NavigationDrawerActivity {
         setContentView(R.layout.activity_about);
         configureNavigationDrawer();
         configureToolbar();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.privacy_policy) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ajapaik.ee"));
+            startActivity(intent);
+        }
     }
 }
